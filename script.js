d871b3c35367eb3569efe24f8c73da86a1d18d2e
Prisca <Prisca@DESKTOP-CQIHODA>
2022-06-23 09:48:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Declare variables
+
+let firstName = "PRISCA"
+let lastName = "OKOCHI"
+let height = 5.2
+let country = "NIGERIA"
+
+//Print the output
+
+alert("Name: My name is " + firstName + " " + lastName + 
+"\n" + "Height: I am " +  height + "fts tall" + 
+"\n" + "Country: I am from " + country);
